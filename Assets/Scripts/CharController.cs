9f0a804476fee7b2583e4971a0fb8012b9930406
Adorimeru <adrimelalvesbraga@gmail.com>
2025-05-09 20:41:02 -0300

    Update CharController.cs

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -3,18 +3,18 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
-	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
-	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
-	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+	[SerializeField] private float m_JumpForce = 400f;							// Força do pulo;
+	//[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100% 
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// Quantidade de suavização;
+	[SerializeField] private bool m_AirControl = false;							// Controle de movimento enquanto estiver no ar;
+	[SerializeField] private LayerMask m_WhatIsGround;							// Máscara para identificar chão;
+	[SerializeField] private Transform m_GroundCheck;							// Marcador de posicionamento para saber se player toca o chão;
+	[SerializeField] private Transform m_CeilingCheck;							// Marcador de posicionamento para saber se player toca o teto;
+	//[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching 
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;            // Whether or not the player is grounded.
-	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+	const float k_GroundedRadius = .2f; // Raio do circulo pra checar se player toca o chão;
+	private bool m_Grounded;            // Se player ta pisando no chão ou não.
+	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up. Raio do circulo 
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 	private Vector3 m_Velocity = Vector3.zero;
@@ -27,8 +27,8 @@ public class CharacterController2D : MonoBehaviour
 	[System.Serializable]
 	public class BoolEvent : UnityEvent<bool> { }
 
-	public BoolEvent OnCrouchEvent;
-	private bool m_wasCrouching = false;
+	//public BoolEvent OnCrouchEvent;
+	//private bool m_wasCrouching = false;
 
 	private void Awake()
 	{
@@ -37,8 +37,8 @@ public class CharacterController2D : MonoBehaviour
 		if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
 
-		if (OnCrouchEvent == null)
-			OnCrouchEvent = new BoolEvent();
+		/*if (OnCrouchEvent == null)
+			OnCrouchEvent = new BoolEvent();*/
 	}
 
 	private void FixedUpdate()
