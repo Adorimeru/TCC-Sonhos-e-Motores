8504db86914279fca7bfa84f1ea0b661dd76bc1b
Adorimeru <adrimelalvesbraga@gmail.com>
2025-05-10 01:50:38 -0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-// Para referenciar o controlador do PJ
-	public CharacterController2D controller;
-
-	public float runSpeed = 40f;
-
-	float horizontalMove = 0f;
-
-	bool jump = false;
+	public CharacterController2D controller; 		// Para referenciar o controlador do player;
+	public float runSpeed = 40f;				// Velocidade padrão do andar;
+	float horizontalMove = 0f;				// Quantitativo de movimento;
+	bool jump = false;					// Pulo settado para falso para que possa ser ativado com a função;
 	
-	// Update is called once per frame
-	void Update () {
-
+	void Update () {	
+		
+		// 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-		if (Input.GetButtonDown("Jump"))
-		{
+		// Se o botão, configurado para "Jump", for pressionado...
+		if (Input.GetButtonDown("Jump")){
+			
+			//... pule!
 			jump = true;
 		}
-
 	}
 
     void FixedUpdate()
     {
-        //Mover personagem]
+        	// Conta que move o personagem;
 		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+	    	// Reconhecendo que o player não está pulando assim que começa a fase;
 		jump = false;
     }
 }
